b22eaa6d249869b8155d19bc16557a24e566fbfa
Cyber1012 <55567338+Cyber1012@users.noreply.github.com>
2020-05-31 19:31:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ var player1 = prompt("Who will be player 1?");
 
 var player2 = prompt("Who will be player 2?");
 
-var player1Sliced = player1.slice(0, 1);
+var player1Pt1 = player1.slice(0, 1);
 
-var player2Sliced = player2.slice(0, 1);
+var player2Pt1 = player2.slice(0, 1);
 
-var player1UpperCased = player1Sliced.toUpperCase();
+var player1UpperCased = player1Pt1.toUpperCase();
 
-var player2UpperCased = player2Sliced.toUpperCase();
+var player2UpperCased = player2Pt1.toUpperCase();
 
-var player1LowerCase = player1.slice(1, player1.length);
+var player1Pt2 = player1.slice(1, player1.length);
 
-var player2LowerCase = player2.slice(1, player2.length);
+var player2Pt2 = player2.slice(1, player2.length);
 
-player1 = player1UpperCased + player1LowerCase;
+var player1LowerCased = player1Pt2.toLowerCase();
 
-player2 = player2UpperCased + player2LowerCase;
+var player2LowerCased = player2Pt2.toLowerCase();
+
+player1 = player1UpperCased + player1LowerCased;
+
+player2 = player2UpperCased + player2LowerCased;
 
 document.querySelectorAll(".p")[0].innerHTML = player1;
 
